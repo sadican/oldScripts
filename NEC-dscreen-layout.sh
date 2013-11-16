5b397e84c9d79873b0846f555b34f0851d9ffea4
Mahmud Rasih ÇELENLİOĞLU <sadican09@gmail.com>
2013-11-16 09:26:39 +0200

    Delete NEC-dscreen-layout.sh

--- NEC-dscreen-layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Date: 28.05.2012
-# Author: sadican
-# Content: Xubuntu 12.04 double screen script for NEC monitor
-
-xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1280x0 --rotate normal --output DP1 --off --output VGA1 --mode 1280x1024 --pos 0x0 --rotate normal
